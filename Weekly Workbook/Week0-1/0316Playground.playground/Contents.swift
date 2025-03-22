879f7c97f6be7808ebbf977ebce769f4fedaeea3
Jimin <gift040404@naver.com>
2025-03-22 17:30:35 +0900

    [feat] Week1 - Workbook

---
+++ Weekly Workbook/Week0-1/0316Playground.playground/Contents.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+var greeting = "UMC 8기 ios 챌린저 김지민\n화이팅!"
+print(greeting)
---
+++ Weekly Workbook/Week0-1/UMC-iOS-Workbook1/UMC-iOS-Workbook1/UMC_iOS_Workbook1App.swift
@@ -0,0 +1,17 @@
+//
+//  UMC_iOS_Workbook1App.swift
+//  UMC-iOS-Workbook1
+//
+//  Created by 김지민 on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct UMC_iOS_Workbook1App: App {
+    var body: some Scene {
+        WindowGroup {
+            TicketView()
+        }
+    }
+}
