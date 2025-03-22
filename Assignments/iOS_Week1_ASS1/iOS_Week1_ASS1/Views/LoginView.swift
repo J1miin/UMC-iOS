636dba1e86813514840b5742b05cc295bfa7c0ed
Jimin <gift040404@naver.com>
2025-03-22 21:54:26 +0900

    [feat] Week1 - ass1

--- Assignments/iOS_Week1_ASS1/iOS_Week1_ASS1/Views/LoginView.swift
+++ Assignments/iOS_Week1_ASS1/iOS_Week1_ASS1/Views/LoginView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct LoginView: View {
     var body: some View {
         VStack{
+            Spacer().frame(height: 104)
             mainTitleGroup
             Spacer().frame(height: 172)
             IdPwdGroup
@@ -21,17 +22,20 @@ struct LoginView: View {
 
 
 private var mainTitleGroup : some View {
-    VStack(alignment:.leading){
+    VStack{
         Group{
             Image("StarbucksLogo1x").resizable()
                 .frame(width: 97, height: 95)
+            Spacer().frame(height: 28)
             Text("안녕하세요.\n스타벅스입니다.")
                 .font(.mainTextExtraBold24)
+                .fixedSize(horizontal: false, vertical: true)
             Spacer().frame(height: 19)
             Text("회원 서비스 이용을 위해 로그인 해주세요")
                 .font(.mainTextMedium16)
                 .foregroundStyle(Color.gray01)
-        }
+        }.padding(.horizontal, 19)
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
@@ -40,30 +44,45 @@ private var IdPwdGroup : some View {
         Group{
             Text("아이디")
                 .font(.mainTextRegular13)
-            Divider()
+                .padding(.horizontal, 19)
+            Divider().padding(.horizontal, 19)
             Spacer().frame(height: 47)
             Text("비밀번호")
                 .font(.mainTextRegular13)
-            Divider()
+                .padding(.horizontal, 19)
+            Divider().padding(.horizontal, 19)
             Spacer().frame(height: 47)
             Image("NormalLogin")
+            .resizable()
+            .frame(width: 364, height: 46)
+            .padding(.horizontal, 19)
+            
         }
     }
 }
 
 private var LoginGroup : some View {
     VStack{
-        Text("이메일로 로그인하기")
+        Text("이메일로 회원가입하기")
             .underline()
             .font(.mainTextRegular12)
             .foregroundStyle(Color.gray04)
         Spacer().frame(height: 19)
-        Image("AppleLogin")
-        Spacer().frame(height: 19)
         Image("KakaoLogin")
+        Spacer().frame(height: 19)
+        Image("AppleLogin")
     }
     
 }
-#Preview {
-    LoginView()
+
+struct SwiftUIView_Preview: PreviewProvider {
+    static var devices = ["iPhone 11", "iPhone 16 Pro"]
+    
+    static var previews: some View {
+        ForEach(devices, id: \.self) { device in
+            LoginView()
+                .previewDevice(PreviewDevice(rawValue: device))
+                .previewDisplayName(device)
+        }
+    }
 }
